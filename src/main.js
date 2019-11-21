66963e1f923cde2d37844f2547d9c064a0f4328e
arloktev <arloktev@mail.ru>
2019-11-21 18:23:30 +0300

    Внес правки из  Pull Request

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const COUNT_TASKS = 3;
+
 const createMenuTemplate = () => {
-  return (
-    `<section class="control__btn-wrap">
+  return `
+    <section class="control__btn-wrap">
       <input
         type="radio"
         name="control"
@@ -29,13 +31,13 @@ const createMenuTemplate = () => {
       <label for="control__statistic" class="control__label"
         >STATISTICS</label
       >
-    </section>`
-  );
+    </section>
+  `;
 };
 
 const createFilterTemplate = () => {
-  return (
-    `<section class="main__filter filter container">
+  return `
+    <section class="main__filter filter container">
       <input
         type="radio"
         id="filter__all"
@@ -102,35 +104,31 @@ const createFilterTemplate = () => {
       <label for="filter__archive" class="filter__label"
         >Archive <span class="filter__archive-count">115</span></label
       >
-    </section>`
-  );
+    </section>
+  `;
 };
 
 const createSortingTemplate = () => {
-  return (
-    `<div class="board__filter-list">
+  return `
+    <div class="board__filter-list">
       <a href="#" class="board__filter">SORT BY DEFAULT</a>
       <a href="#" class="board__filter">SORT BY DATE up</a>
       <a href="#" class="board__filter">SORT BY DATE down</a>
-    </div>`
-  );
+    </div>
+  `;
 };
 
-const createBoardContainer = () => {
-  return (
-    `<section class="board container"></section>`
-  );
+const createBoardTemplate = () => {
+  return `<section class="board container"></section>`;
 };
 
 const createTaskListTemplate = () => {
-  return (
-    `<div class="board__tasks"></div>`
-  );
+  return `<div class="board__tasks"></div>`;
 };
 
 const createTaskTemplate = () => {
-  return (
-    `<article class="card card--black">
+  return `
+    <article class="card card--black">
       <div class="card__form">
         <div class="card__inner">
           <div class="card__control">
@@ -194,13 +192,13 @@ const createTaskTemplate = () => {
           </div>
         </div>
       </div>
-    </article>`
-  );
+    </article>
+  `;
 };
 
 const createTaskEditTemplate = () => {
-  return (
-    `<article class="card card--edit card--yellow card--repeat">
+  return `
+    <article class="card card--edit card--yellow card--repeat">
       <form class="card__form" method="get">
         <div class="card__inner">
           <div class="card__color-bar">
@@ -454,14 +452,12 @@ const createTaskEditTemplate = () => {
           </div>
         </div>
       </form>
-    </article>`
-  );
+    </article>
+  `;
 };
 
 const createLoadMoreButtonTemplate = () => {
-  return (
-    `<button class="load-more" type="button">load more</button>`
-  );
+  return `<button class="load-more" type="button">load more</button>`;
 };
 
 const renderComponent = (container, template, place = `beforeend`) => {
@@ -473,21 +469,17 @@ const control = main.querySelector(`.main__control`);
 
 renderComponent(control, createMenuTemplate());
 renderComponent(main, createFilterTemplate());
-renderComponent(main, createBoardContainer());
+renderComponent(main, createBoardTemplate());
 
 const board = main.querySelector(`.board`);
 renderComponent(board, createSortingTemplate());
 renderComponent(board, createTaskListTemplate());
 
-const boardTasks = main.querySelector(`.board__tasks`);
+const boardTasks = board.querySelector(`.board__tasks`);
 renderComponent(boardTasks, createTaskEditTemplate());
 
-const countTasks = 3;
-
-new Array(countTasks)
-  .fill(``)
-  .forEach(
-      () => renderComponent(boardTasks, createTaskTemplate())
-  );
+for (let i = 0; i < COUNT_TASKS; i++) {
+  renderComponent(boardTasks, createTaskTemplate());
+}
 
 renderComponent(board, createLoadMoreButtonTemplate());
